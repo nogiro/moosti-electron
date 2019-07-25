bc7d3fdde7346c7c9b99c0c67dd202b18d13bac5
nogiro <nogiro.temp@gmail.com>
2019-07-25 19:51:27 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+
+let win : BrowserWindow | null = null;
+function createWindow() {
+  console.info("createWindow()");
+  if (win === null) {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    win.on('closed', () => {
+      win = null
+    });
+
+    win.loadURL('https://www.moosti.com/');
+  }
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  };
+});
+
+app.on('activate', () => {
+  createWindow();
+});
+
