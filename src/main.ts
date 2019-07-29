52ea6f4a99b04872b9f4d12735b5221559669989
nogiro <nogiro.temp@gmail.com>
2019-07-29 19:36:43 +0900

    別ページを開けないように

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import { app, BrowserWindow } from 'electron';
 
 let win : BrowserWindow | null = null;
 function createWindow() {
-  if (win === null) {
-    win = new BrowserWindow({ width: 800, height: 600 });
-    win.on('closed', () => {
-      win = null
-    });
-
-    win.loadURL('https://www.moosti.com/');
+  if (win !== null) {
+    return;
   }
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+  });
+
+  win.on('closed', () => {
+    win = null
+  });
+
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+  });
+  win.loadURL('https://www.moosti.com');
 }
 
 app.on('ready', () => {
